6249dac64d06a437efb5ec6cca984524282e4f5e
Shivam Raj <shivamrj83@gmail.com>
2019-07-18 21:11:36 +0530

    for web hosting

--- frontend/js/submitOptions.js
+++ frontend/js/submitOptions.js
@@ -1,4 +1,4 @@
-
+localStorage.clear();
 function userLoginForm(){
     document.getElementById('service').innerHTML=`<div class="container">
     <div class="row d-flex justify-content-center">
@@ -13,9 +13,9 @@ function userLoginForm(){
         <div class="col-lg-4"></div>
         <div class="col-lg-5 offset-lg-2">
           <label for="email">Email</label>
-            <input type="email" id="userEmail" name="email" />
+            <input  required="true" type="email" id="userEmail" name="email" />
             <label for="userPassword">Password:</label>
-            <input
+            <input required="true"
             type="password"
             id="userPassword"
             name="password"
@@ -43,9 +43,9 @@ function ngoLoginForm(){
         <div class="col-lg-4"></div>
         <div class="col-lg-5 offset-lg-2">
           <label for="email">Email</label>
-            <input type="email" id="ngoEmail" name="email" />
+            <input required="true" type="email" id="ngoEmail" name="email" />
             <label for="password">Password:</label>
-            <input
+            <input required="true"
             type="password"
             id="ngoPassword"
             name="password"
@@ -73,9 +73,9 @@ function restaurantLoginForm(){
         <div class="col-lg-4"></div>
         <div class="col-lg-5 offset-lg-2">
           <label for="email">Email</label>
-            <input type="email" id="restEmail" name="email" />
+            <input required="true" type="email" id="restEmail" name="email" />
             <label for="password">Password:</label>
-            <input
+            <input required="true"
             type="password"
             id="restPassword"
             name="password"
@@ -103,11 +103,11 @@ function staffLoginForm(){
         <div class="col-lg-4"></div>
         <div class="col-lg-5 offset-lg-2">
           <label for="email">Email</label>
-            <input type="email" id="staffEmail" name="email" />
+            <input required="true" type="email" id="staffEmail" name="email" />
             <label for="staffPassword">Password:</label>
-            <input
+            <input required="true"
             type="password"
-            id="password"
+            id="staffPassword"
             name="password"
             />
 
@@ -138,28 +138,28 @@ function userSignUpForm(){
         <div class="col-lg-4"></div>
         <div class="col-lg-5 offset-lg-2">
             <label for="userSignFirstname">Firstname</label>
-            <input
+            <input required="true"
             type="text"
             id="userSignFirstname"
             name="firstname"
             />
 
             <label for="userLastname">Lastname</label>
-            <input type="text" id="userSignLastname" name="lastname" />
+            <input required="true" type="text" id="userSignLastname" name="lastname" />
 
             <label for="userUsername">Username</label>
-            <input type="text" id="userSignUsername" name="userUsername" />
+            <input required="true" type="text" id="userSignUsername" name="userUsername" />
 
             <label for="Date of Birth">Date of Birth</label>
-            <input type="text" id="userSignDateofBirth" name="Date of Birth" />
+            <input required="true" type="text" id="userSignDateofBirth" name="Date of Birth" />
 
             <label for="email">Email</label>
             <input type="email" id="userSignEmail" name="email" style="width:68%;">&nbsp<input type="submit" onclick="userotp()" class="btn btn-info" style="height:7.2%;" value="Generate OTP">
-            <div id="userotp">
+            <div id="userOtp">
             </div>
 
             <label for="userPassword">Password:</label>
-            <input
+            <input required="true"
             type="password"
             id="userSignPassword"
             name="password"
@@ -172,12 +172,26 @@ function userSignUpForm(){
     </div>
 </div>	`;
 }
+
 function userotp(){
-  document.getElementById("userotp").innerHTML=`
+
+  document.getElementById("userOtp").innerHTML=`
     <label for="otp">Enter OTP</label>
     <input type="text" id="generateduserotp" name="userotp" style="width:35%">
   `
+    let email=document.getElementById("userSignEmail").value;
+    axios.post("http://localhost:3000/user/verify",{
+        email:email
+    }).then(res=>{
+        console.log("hello")
+    }).catch(err=>{
+        console.log(err)
+    })
+
+
+
 }
+
 function ngoSignUpForm(){
     document.getElementById('service').innerHTML=`<div class="container">
     <div class="row d-flex justify-content-center">
@@ -192,28 +206,26 @@ function ngoSignUpForm(){
         <div class="col-lg-4"></div>
         <div class="col-lg-5 offset-lg-2">
         <label for="firstname">NGO Name</label>
-        <input
+        <input required="true"
         type="text"
         id="ngoSignName"
         name="ngoName"
         />
 
         <label for="lastname">NGO Code</label>
-        <input type="text" id="ngoSignCode" name="ngoCode" />
+        <input required="true" type="text" id="ngoSignCode" name="ngoCode" />
 
         <label for="username">Established Date</label>
-        <input type="text" id="dateOfEstd" name="ngoDateOfEstd" placeholder="DD/MM/YY"/>
-
+        <input required="true" type="text" id="dateOfEstd" name="ngoDateOfEstd" placeholder="DD/MM/YY"/>
 
 
         <label for="email">Email</label>
-        <input type="email" id="ngoSignEmail" name="email" style="width:68%;">&nbsp<input type="submit" onclick="ngootp()" class="btn btn-info" style="height:8%;" value="Generate OTP">
-        <div id="ngootp">
-        </div>
-
+              <input type="email" id="ngoSignEmail" name="email" style="width:68%;">&nbsp<input type="submit" onclick="ngootp()" class="btn btn-info" style="height:8%;" value="Generate OTP">
+              <div id="ngoOtp">
+              </div>
 
         <label for="password">Password:</label>
-        <input
+        <input required="true"
         type="password"
         id="ngoSignPassword"
         name="password"
@@ -225,12 +237,23 @@ function ngoSignUpForm(){
     </div>
 </div>	`;
 }
+
 function ngootp(){
-  document.getElementById("ngootp").innerHTML=`
+  document.getElementById("ngoOtp").innerHTML=`
     <label for="otp">Enter OTP</label>
     <input type="text" id="generatedngootp" name="ngootp" style="width:35%">
   `
+
+  let email=document.getElementById("ngoSignEmail").value;
+    axios.post("http://localhost:3000/ngo/verify",{
+        email:email
+    }).then(res=>{
+        console.log("hello")
+    }).catch(err=>{
+        console.log(err)
+    })
 }
+
 
 function restaurantSignUpForm(){
     document.getElementById('service').innerHTML=`<div class="container">
@@ -246,7 +269,7 @@ function restaurantSignUpForm(){
         <div class="col-lg-4"></div>
         <div class="col-lg-5 offset-lg-2">
         <label for="restName">Name of the Restaurant</label>
-        <input
+        <input required="true"
         type="text"
         id="restSignName"
         name="restName"
@@ -257,16 +280,15 @@ function restaurantSignUpForm(){
         <div id="resotp">
         </div>
 
-
         <label for="password">Password:</label>
-        <input
+        <input required="true"
         type="password"
         id="restSignPassword"
         name="password"
         />
 
         <label for="lastname">Number of Persons that can be fed.</label>
-        <input
+        <input required="true"
         type="text"
         id="foodAvailable"
         name="foodAvailable"
@@ -279,6 +301,7 @@ function restaurantSignUpForm(){
     </div>
 </div>`;
 }
+
 function resotp(){
   document.getElementById("resotp").innerHTML=`
     <label for="otp">Enter OTP</label>
@@ -295,10 +318,20 @@ function userLogin(){
     let userEmail=document.getElementById('userEmail').value;
     let userPassword=document.getElementById('userPassword').value;
 
-    axios.post("",{
-        userEmail,
-        userPassword
+    axios.post("http://localhost:3000/user/login",{
+        email:userEmail,
+        password:userPassword
     }).then(res=>{
+        if(res.data.message==="Authentication successful")
+            {   console.log(res.data)
+                localStorage.setItem('email',userEmail);
+                localStorage.setItem('userName',res.data.userName);
+                localStorage.setItem('userType',"user")
+                window.open('feed.html',"_self");
+            }
+        else{
+            alert(res.data.message);
+        }
 
     }).catch(err=>{
         alert(err);
@@ -311,11 +344,20 @@ function ngoLogin(){
     let ngoPassword=document.getElementById('ngoPassword').value;
 
 
-    axios.post("",{
+    axios.post("http://localhost:3000/ngo/login",{
         email:ngoEmail,
         password:ngoPassword
     }).then(res=>{
-
+        if(res.data.message==="Authentication successful")
+            {
+                localStorage.setItem('email',ngoEmail);
+                localStorage.setItem('userName',res.data.userName);
+                localStorage.setItem('userType',"ngo");
+                window.open('feed.html',"_self");
+            }
+        else{
+            alert(res.data.message);
+        }
     }).catch(err=>{
         alert(err);
     })
@@ -342,11 +384,20 @@ function staffLogin(){
     let staffEmail=document.getElementById('staffEmail').value;
     let staffPassword=document.getElementById('staffPassword').value;
 
-    axios.post("",{
+    axios.post("http://localhost:3000/staff/login",{
         email:staffEmail,
         password:staffPassword
     }).then(res=>{
-
+        if(res.data.message==="Authentication successful")
+            {
+                localStorage.setItem('email',staffEmail);
+                localStorage.setItem('userType',"staff")
+                localStorage.setItem('userName',res.data.userName);
+                window.open('staffLogin.html',"_self");
+            }
+        else{
+            alert(res.data.message);
+        }
     }).catch(err=>{
         alert(err);
     })
@@ -361,46 +412,128 @@ function userSignUp(){
     let userDateofBirth=document.getElementById('userSignDateofBirth').value;
     let userEmail=document.getElementById('userSignEmail').value;
     let userPassword=document.getElementById('userSignPassword').value;
-
-
-    axios.post("http://localhost:3000/user/login",{
+    let userOtp=document.getElementById('generateduserotp').value;
+    if(userOtp==null)
+    {
+        alert("enter otp")
+    }
+    else{
+    axios.post("http://localhost:3000/user/",{
         firstname:userFirstname,
         lastname:userLastname,
         username:userUsername,
-        dateofBirth:userDateofBirth,
+        dateOfBirth:userDateofBirth,
         email:userEmail,
-        password:userPassword
+        password:userPassword,
+        userOtp:userOtp
     }).then(res=>{
-        console.log("done")
+        if(res.data.message==="Account Created")
+        {
+            console.log("aaya hu yaha")
+            localStorage.setItem('userName',userUsername);
+            localStorage.setItem('email',userEmail);
+            localStorage.setItem('userType',"user")
+            window.open('feed.html',"_self");
+
+        }
+        else{
+           console.log(res);
+           document.getElementById('service').innerHTML=`<div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="menu-content pb-70 col-lg-8">
+                        <div class="title text-center">
+                            <h1 class="mb-10">Sign Up as User</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4"></div>
+                    <div class="col-lg-5 offset-lg-2">
+                        <label for="userSignFirstname">Firstname</label>
+                        <input required="true"
+                        type="text"
+                        id="userSignFirstname"
+                        name="firstname"
+                        />
+
+                        <label for="userLastname">Lastname</label>
+                        <input required="true" type="text" id="userSignLastname" name="lastname" />
+
+                        <label for="userUsername">Username</label>
+                        <input required="true" type="text" id="userSignUsername" name="userUsername" />
+
+                        <label for="Date of Birth">Date of Birth</label>
+                        <input required="true" type="text" id="userSignDateofBirth" name="Date of Birth" />
+
+                        <label for="email">Email</label>
+                        <input type="email" id="userSignEmail" name="email" style="width:68%;">&nbsp<input type="submit" onclick="userotp()" class="btn btn-info" style="height:7.2%;" value="Generate OTP">
+                        <div id="userOtp">
+                        </div>
+
+                        <label for="userPassword">Password:</label>
+                        <input required="true"
+                        type="password"
+                        id="userSignPassword"
+                        name="password"
+                        />
+
+                    <button class="btn btn-info" onclick="userSignUp()">Submit</button>
+                    </div>
+
+
+                </div>
+            </div>	`;
+            alert(res.data.message);
+        }
+
     }).catch(err=>{
-        console.log("error")
+        console.log(err)
     })
 
-
+    }
 
 }
 
 function ngoSignUp(){
-
+    console.log
     let ngoName=document.getElementById('ngoSignName').value;
     let ngoCode=document.getElementById('ngoSignCode').value;
     let dateOfEstd=document.getElementById('dateOfEstd').value;
     let email=document.getElementById('ngoSignEmail').value;
     let password=document.getElementById('ngoSignPassword').value;
-
-
-    axios.post("",{
-        ngoName,
-        ngoCode,
-        dateOfEstd,
-        email,
-        password
+    let ngoOtp=document.getElementById('generatedngootp').value;
+    if(ngoOtp==null)
+    {
+        alert("enter otp")
+    }
+    else{
+    axios.post("http://localhost:3000/ngo",{
+        ngoName:ngoName,
+        ngoCode:ngoCode,
+        dateOfEstd:dateOfEstd,
+        email:email,
+        password:password,
+        ngoOtp:ngoOtp
     }).then(res=>{
+        console.log(res)
+        if(res.data.message==="Account Created")
+        {
 
-    }).catch(res=>{
+            localStorage.setItem('email',email);
+            localStorage.setItem('userName',ngoName);
+            localStorage.setItem('userType',"ngo");
+            window.open('feed.html',"_self");
+        }
+        else{
+            console.log("else")
+            alert(res.data.message);
+        }
 
+    }).catch(err=>{
+        alert(err);
     })
-
+    }
 }
 
 function restSignUp(){
